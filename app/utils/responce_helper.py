3291ab833350f490368d5175a434b001f93852c0
jahnavim708-art <jahnavim708@gmail.com>
2026-06-04 18:10:29 +0530

    utility module that standardizes API responses

---
+++ app/utils/responce_helper.py
@@ -0,0 +1,14 @@
+def success_response(data=None, message="Success"):
+    return {
+        "success": True,
+        "message": message,
+        "data": data
+    }
+
+
+def error_response(message="Error"):
+    return {
+        "success": False,
+        "message": message,
+        "data": None
+    }
