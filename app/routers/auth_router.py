9478672258bb9740c44f3f3a23c7cc0adba3bcd2
jahnavim708-art <jahnavim708@gmail.com>
2026-06-04 18:02:03 +0530

    It handles user login and token generation..

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 from fastapi import Depends
-from fastapi.security import OAuth2PasswordRequestForm
-
+#from fastapi.security import OAuth2PasswordRequestForm
+from app.schemas.auth import LoginRequest, TokenResponse
 from sqlalchemy.orm import Session
-
+from fastapi import HTTPException, status
 from app.config.database import get_db
 from app.services.auth_service import AuthService
 
@@ -13,13 +13,20 @@ router = APIRouter(
 )
 
 
-@router.post("/login")
+@router.post("/login", response_model=TokenResponse)
 def login(
-    form_data: OAuth2PasswordRequestForm = Depends(),
+    request: LoginRequest,
     db: Session = Depends(get_db)
 ):
-    return AuthService.login(
+    result= AuthService.login(
         db,
-        form_data.username,
-        form_data.password
+        request.username,
+        request.password
     )
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid username or password"
+        )
+
+    return result
